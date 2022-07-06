917dcc67d4cca008244f2e7d9112a3892d9c5768
Jesse santoz <j-esse@hotmail.com.br>
2022-07-05 21:02:07 -0300

    function onclick atualizar

---
+++ js/dados.js
@@ -0,0 +1,22 @@
+const dados = [{
+    nome: 'Pedro',
+    titulo: 'Cbum se pronuncia sobre Shaw',
+    data: '02/03/2000',
+    sub: 'iabgvuid baviuo bauidbv asbvuasb duvybas',
+    paragrafo: 'blabla bla blabalblablabal blabla bla blabalblablabal 1'
+},
+{
+    nome: 'João',
+    titulo: 'Show de Cariani e Balestrin promete',
+    data: '07/12/2050',
+    sub: 'isbfgoiubsgoifdoi ubgvsfuoibv isuo bfvbsdfiuovbiuodf',
+    paragrafo: 'blabla bla blabalblablabal  blabla bla blabalblablabal blabla bla blabalblablabal 2'
+},
+{
+    nome: 'Lucas',
+    titulo: 'Ramon e Urs são as novas promessas',
+    data: '30/05/2021',
+    sub: 'ohjfaoshvbauyfcdjnasvcuas ubasdvj uydvba sdyvbjhda ',
+    paragrafo: 'blabla bla blabalblablabal blabla bla blabalblablabal blabla bla blabalblablabal blabla bla blabalblablabal 3'
+}
+]
---
+++ js/noticias.js
@@ -0,0 +1,8 @@
+function atualizar(n) {
+    document.getElementById("nome").textContent = dados[n].nome
+    document.getElementById("title").textContent = dados[n].titulo
+    document.getElementById("data").textContent = dados[n].data
+    document.getElementById("sub").textContent = dados[n].sub
+    document.getElementById("parag").textContent = dados[n].paragrafo
+    document.getElementById("parag1").textContent = dados[n].paragrafo
+}
